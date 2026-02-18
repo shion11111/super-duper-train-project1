aa1fe2261191e2bc2d8aaf6d30d52d40710e8085
shion11111 <skishaba@una.edu>
2026-02-18 11:54:02 -0600

    Add files via upload

---
+++ super-duper-train-project1-project-attempt-1/pages/data.js
@@ -0,0 +1,41 @@
+const jsObjectData = [
+/* 🌐 */
+    { id: 1, continent: "North America", country: "The United States", city: "New York"},
+    { id: 2, continent: "North America", country: "Canada", city: "Toronto"},
+    { id: 3, continent: "Europe", country: "United Kingdom", city: "London"},
+    { id: 4, continent: "Europe", country: "France", city: "Paris"},
+    { id: 5, continent: "Asia", country: "Japan", city: "Tokyo"},
+    { id: 6, continent: "Asia", country: "China", city: "Beijing"},
+   
+]
+
+// NATO PHONETIC ALPHABET
+
+const jsArrayData = [
+  "Alfa",
+  "Bravo",
+  "Charlie",
+  "Delta",
+  "Echo",
+  "Foxtrot",
+  "Golf",
+  "Hotel",
+  "India",
+  "Juliett",
+  "Kilo",
+  "Lima",
+  "Mike",
+  "November",
+  "Oscar",
+  "Papa",
+  "Quebec",
+  "Romeo",
+  "Sierra",
+  "Tango",
+  "Uniform",
+  "Victor",
+  "Whiskey",
+  "X-ray",
+  "Yankee",
+  "Zulu"
+];
